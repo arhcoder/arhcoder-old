e41754c05840c732f071b9efae668b66e882e528
Alejandro Ramos <arhcoder@gmail.com>
2021-05-01 22:46:28 -0500

    Aclaratorias con alerts añadidas en ambas páginas, para recalar que la web está aún en construcción...

---
+++ 03-Scripts/aclaratoria_index.js
@@ -0,0 +1,9 @@
+// Lanza un alert, explicando que la página no es adaptable a todas las pantallas,
+// pero la siguiente sí. En general, el portafolio permance aún en construcción
+// (Mayo 1, 2021)...
+
+window.onload = () =>
+{
+    alert("PORTAFOLIO EN CONSTRUCCIÓN:\n\nEsta primera página AÚN no está adaptada para pantallas móviles.\n");
+    alert("Mi nombre es Alejandro Ramos, soy un desarrollador de software aficionado a la música y el diseño. Me encanta aprender de todo y crear de todo. Déjame mostrarte...");
+}
---
+++ 03-Scripts/aclaratoria_main.js
@@ -0,0 +1,8 @@
+// Lanza un alert en el que notifica que el sitio aún está en construcción,
+// e invita al usuario a visitar el perfil de GitHub, el portafolio permance
+// aún en construcción (Mayo 1, 2021)...
+
+window.onload = () =>
+{
+    alert("PORTAFOLIO EN CONSTRUCCIÓN\n\nPara conocer mis proyectos, te invito a mi perfil de GitHub:\n\ngithub.com/arhcoder\n\n y a mis redes sociales...");
+}
